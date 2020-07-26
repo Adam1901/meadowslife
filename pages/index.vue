4a04f3a3b661870c30a4a2108abf2c9dce0d597f
play.js <git@gituser.com>
2020-07-26 15:20:42 +0100

    formatting

--- pages/index.vue
+++ pages/index.vue
@@ -5,32 +5,44 @@
         <!--        https://github.com/apertureless/vue-parallax/issues/10-->
         <div style="position: relative; z-index: 9999; background-color: #fff;">
           <h1 style="margin:0;">Meadows Wedding</h1>
-          <h2 style="margin:0;">March 2020</h2>
+          <h2 style="margin:0;">19<small>th</small> March 2022</h2>
           <parallax :speedFactor="speedFactorSpeed">
             <img src="/brighton.jpg" alt="very cool bg">
           </parallax>
         </div>
 
-        <div style="background-color: #fff; height: 100vh;"></div>
-        <h1>Test</h1>
+        <div style="background-color: #fff; height: 50vh;">
+          <h1>Blank body</h1>
+        </div>
 
         <div style="position: relative;">
           <parallax :speedFactor="speedFactorSpeed" :fixed="true">
             <img src="/engadgement.jpg" alt="very cool bg">
           </parallax>
         </div>
-        <parallax :speedFactor="speedFactorSpeed">
-          <img src="/leggies.jpg" alt="very cool bg">
-        </parallax>
+        
+        <div style="position: relative; z-index: 9999; background-color: #fff;">
+          <parallax :speedFactor="speedFactorSpeed">
+            <img src="/leggies.jpg" alt="very cool bg">
+          </parallax>
+        </div>
+        
+        
+        <div style="background-color: #fff; height: 50vh;">
+          <h1>Blank body</h1>
+        </div>
 
-        <parallax :speedFactor="speedFactorSpeed">
-          <img src="/prague.jpg" alt="very cool bg">
-        </parallax>
+        <div style="position: relative;">
+          <parallax :speedFactor="speedFactorSpeed" :fixed="false">
+            <img src="/prague.jpg" alt="very cool bg">
+          </parallax>
+        </div>
 
-        <parallax :speedFactor="speedFactorSpeed">
-          <img src="/poser.jpg" alt="very cool bg">
-        </parallax>
-
+        <div style="position: relative;">
+          <parallax :speedFactor="speedFactorSpeed">
+            <img src="/poser.jpg" alt="very cool bg">
+          </parallax>
+        </div>
         <!--      <parallax :speedFactor="{speedFactor}">-->
         <!--        <img src="/ballet.jpg" alt="very cool bg">-->
         <!--      </parallax>-->
