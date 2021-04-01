033af9892456ddacdb5bbd81dba6f942300aa7f2
Adam Meadows <Adam@jchendrick,com>
2021-04-01 16:12:01 +0100

    Added countdown

---
+++ components/countdown.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+   <h1>Numbers of days until the magical day {{ daysUntil }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "countdown",
+  data: function () {
+    return {
+      daysUntil: 0
+    }
+  },
+  mounted() {
+    const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+    const wedding = new Date(2022, 2, 19);
+    const now = new Date();
+    this.daysUntil = Math.round(Math.abs((now - wedding) / oneDay));
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
